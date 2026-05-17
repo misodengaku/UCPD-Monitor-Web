965c52e0a15b2415b6849e4edc2fdb6df8d74f88
misodengaku <misodengaku@gmail.com>
2026-05-18 01:06:20 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { readFileSync } from 'fs'
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'url'
+import { resolve } from 'path'
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
-const pkg = JSON.parse(readFileSync(resolve(__dirname, '../package.json'), 'utf-8'))
+const pkg = JSON.parse(readFileSync(resolve('./package.json'), 'utf-8'))
 
 // https://vite.dev/config/
 export default defineConfig({
